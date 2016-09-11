b00543ed7c1dbfe3823c3be4c90504e06811d6a5
David C <SadFud@users.noreply.github.com>
2016-09-11 23:26:18 +0200

    Add files via upload

---
+++ Random Icon/Random Icon/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(209, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(139, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar icono"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "C:\iconorandom.ico"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(1, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ruta de generacion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Tamaño del icono"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"32x32", "48x48", "52x52"})
+        Me.ComboBox1.Location = New System.Drawing.Point(117, 49)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(86, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(5, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(343, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Funcion creada por SadFud - Codigo disponible en github.com/SadFud"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(360, 96)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Generar icono aleatorio"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Random Icon/Random Icon/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rutaicono As String = TextBox1.Text
+        Dim tamaño As String
+        If ComboBox1.Text = "32x32" Then
+            tamaño = "33"
+        End If
+        If ComboBox1.Text = "48x48" Then
+            tamaño = "49"
+        End If
+        If ComboBox1.Text = "52x52" Then
+            tamaño = "53"
+        End If
+        If File.Exists(rutaicono) Then
+            File.Delete(rutaicono)
+            Call generateicon()
+            MsgBox("Icono generado en " & TextBox1.Text & " satisfactoriamente.")
+        Else
+            Call generateicon()
+            MsgBox("Icono generado en " & TextBox1.Text & " satisfactoriamente.")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Public Function generateicon() As String
+        Dim tamaño As String
+        If ComboBox1.Text = "32x32" Then
+            tamaño = "33"
+        End If
+        If ComboBox1.Text = "48x48" Then
+            tamaño = "49"
+        End If
+        If ComboBox1.Text = "52x52" Then
+            tamaño = "53"
+        End If
+        Dim ancho As Integer = tamaño
+        Dim alto As Integer = tamaño
+        Dim bmp As New Bitmap(ancho, alto)
+        Dim rand As Random = New Random()
+        For y As Integer = 0 To alto - 1
+            For x As Integer = 0 To ancho - 1
+                Dim a As Integer = rand.Next(256)
+                Dim r As Integer = rand.Next(256)
+                Dim g As Integer = rand.Next(256)
+                Dim b As Integer = rand.Next(256)
+                bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b))
+            Next
+        Next
+        Dim HIcon As IntPtr = bmp.GetHicon()
+        Dim nuevoicono As Icon = Icon.FromHandle(HIcon)
+
+        ' Dim rutafinal As String = Path.GetTempPath() + "iconorandom.ico"
+        Dim rutafinal As String = TextBox1.Text
+        Dim oFileStream As FileStream = New IO.FileStream(rutafinal, FileMode.CreateNew)
+        nuevoicono.Save(oFileStream)
+        oFileStream.Close()
+        Return rutafinal
+    End Function
+End Class
